82ef67b80edd83952592fd754a779ab14da32974
Bárbara Briceño <programbarb07@gmail.com>
2025-11-05 00:03:15 -0400

    archivo start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navegar a la carpeta del proyecto
+cd C:\laragon\www\BD3_BB\Class8_BD3_BB\nodeJs_MySQL_RestAPI_bb
+
+# Instalar dependencias
+npm install
+
+# Iniciar la aplicación
+npm run start
